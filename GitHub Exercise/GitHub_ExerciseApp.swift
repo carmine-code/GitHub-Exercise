d752fbe996a1f18fa5f1354ba516bd40ad4240ef
Carmine Loschiavo <marketing@zoominternet.net>
2023-01-16 10:24:44 -0500

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Carmine Loschiavo on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
